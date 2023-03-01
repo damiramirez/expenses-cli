69a6adfec4c02592079518bf07e348d43e29754c
Damian Ramirez <damianagustinrz@gmail.com>
2023-03-01 18:50:36 -0300

    average added

--- commands/commands.go
+++ commands/commands.go
@@ -25,22 +25,41 @@ func GetInput() (string, error) {
 }
 
 func ShowInConsole(expensesList []float32) {
-	
+	fmt.Println(contentString(expensesList))
+}
+
+func contentString(expensesList []float32) string {
 	builder := strings.Builder{}
+
+	max, min, total, average := expensesDetails(expensesList)
 
 	fmt.Println(" ----------------- ")
 	for i, exp := range expensesList {
 		builder.WriteString(fmt.Sprintf("Expense: %6.2f\n", exp))
 
 		if i == len(expensesList) - 1 {
-			fmt.Println(" ----------------- ")
-			builder.WriteString(fmt.Sprintf("Total: %6.2f\n", expenses.Sum(expensesList...)))
-			builder.WriteString(fmt.Sprintf("Max: %6.2f\n", expenses.Max(expensesList...)))
-			builder.WriteString(fmt.Sprintf("Min: %6.2f\n", expenses.Min(expensesList...)))
+			builder.WriteString(fmt.Sprintf("Total: %6.2f\n", total))
+			builder.WriteString(fmt.Sprintf("Average: %6.2f\n", average))
+			builder.WriteString(fmt.Sprintf("Max: %6.2f\n", max))
+			builder.WriteString(fmt.Sprintf("Min: %6.2f\n", min))
 		}
 	}
 
-	fmt.Println(builder.String())
+	return builder.String()
+}
+
+func expensesDetails(expensesList []float32) (max, min, total, average float32) {
+
+	if len(expensesList) == 0 {
+		return
+	}
+
+	min = expenses.Min(expensesList...)
+	max = expenses.Max(expensesList...)
+	total = expenses.Sum(expensesList...)
+	average = expenses.Average(expensesList...)
+
+	return
 }
 
 func Export() {
